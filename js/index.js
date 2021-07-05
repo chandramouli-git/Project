c3df53a55b1b16c92181a19295738454ec817fbe
chandramouli-git <60184634+chandramouli-git@users.noreply.github.com>
2021-07-05 15:27:31 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -124,7 +124,7 @@ function log(message) {
          $.ajax({
           type: "POST",
           contentType: "application/json ; charset=utf-8",
-          url: "http://localhost:3000/check?x="+urlParams.get('x'),
+          url: "./check?x="+urlParams.get('x'),
           dataType: "json",
         data: JSON.stringify({id : hash,amt : document.getElementById("amt").value,val:urlParams.get('x')})
           
